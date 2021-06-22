361fe1fccd16fe04590aed9d87f2d9d3e6f5e821
jorge <jlucesan@gmail.com>
2021-06-22 21:41:28 +0200

    Files from previous commit.

--- src/Controller/HomeController.java
+++ src/Controller/HomeController.java
@@ -1,125 +1,27 @@
 package Controller;
 
-import Model.Game;
-import Model.GameDAO;
-import Model.PurchasableItem;
-import View.GameForm;
-import View.GameInfo;
 import View.Home;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.LinkedList;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HomeController implements ChangeListener {
-
-    private static final String PENDING_TAB = "Pending";
-    private static final String PURCHASED_TAB = "Purchased";
+public class HomeController implements ActionListener {
 
     private final Home _viewHome;
-    private final GameDAO _gameDAO;
 
-    private DefaultListModel<Game> _defaultListGamesModel;
-
-    public HomeController(Home viewHome) {
+    public HomeController(Home viewHome){
 
         _viewHome = viewHome;
-        _gameDAO = new GameDAO();
-
-        _viewHome.tabbedPanel.addChangeListener(this);
-        _viewHome.addGameB.addActionListener(e -> openGameForm());
     }
 
-    public void loadTab() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-        if (_viewHome.tabbedPanel.getSelectedIndex() != -1) {
+        if(e.getSource() == _viewHome.BooksB){
 
-            _defaultListGamesModel = new DefaultListModel<>();
-            List<Game> gameList = getGamesList();
-            for (Game game : gameList) {
-
-                _defaultListGamesModel.addElement(game);
-            }
-
-            JList<Game> gameJList = new JList<>(_defaultListGamesModel);
-            gameJList.setCellRenderer(new GameRenderer());
-            gameJList.addMouseListener(new MouseListener() {
-                public void mouseClicked(MouseEvent e) {
-
-                    GameInfo gi = new GameInfo("GameInfo");
-                    int selectedGameId = gameJList.getSelectedValue().getId();
-
-                    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                    gi.setSize((int) screenSize.getWidth() * 2 / 3, (int) screenSize.getHeight() * 2 / 3);
-
-                    GameInfoController gameInfoController = new GameInfoController(gi, selectedGameId);
-                    gameInfoController.setDefaultListModel(_defaultListGamesModel);
-
-                    gi.setVisible(true);
-                }
-
-                public void mousePressed(MouseEvent e) {
-                }
-
-                public void mouseReleased(MouseEvent e) {
-                }
-
-                public void mouseEntered(MouseEvent e) {
-                }
-
-                public void mouseExited(MouseEvent e) {
-                }
-            });
-
-            gameJList.setBackground(new Color(156, 143, 231));
-            JScrollPane jsp = (JScrollPane) _viewHome.tabbedPanel.getSelectedComponent();
-            jsp.setViewportView(gameJList);
         }
-    }
+        else{ //_viewHome.GamesB
 
-    private List<Game> getGamesList() {
-
-        List<Game> gameList = new LinkedList<>();
-        if (_viewHome.tabbedPanel.getTitleAt(_viewHome.tabbedPanel.getSelectedIndex()).equals(PENDING_TAB)) {
-
-            gameList = loadPendingGames();
-        } else if (_viewHome.tabbedPanel.getTitleAt(_viewHome.tabbedPanel.getSelectedIndex()).equals(PURCHASED_TAB)) {
-
-            gameList = loadPurchasedGames();
         }
-        return gameList;
-    }
-
-    private List<Game> loadPendingGames() {
-
-        return _gameDAO.getByState(PurchasableItem.ItemState.Pending.toString());
-    }
-
-    private List<Game> loadPurchasedGames() {
-
-        return _gameDAO.getByState(PurchasableItem.ItemState.Purchased.toString());
-    }
-
-    public void stateChanged(ChangeEvent e) {
-
-        loadTab();
-    }
-
-    private void openGameForm() {
-
-        GameForm gf = new GameForm("AddGame");
-
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        gf.setSize((int) screenSize.getWidth() * 2 / 3, (int) screenSize.getHeight() * 2 / 3);
-
-        GameFormController controller = new GameFormController(gf);
-        controller.setDefaultListModel(_defaultListGamesModel);
-
-        gf.setVisible(true);
     }
 }
---
+++ src/Controller/ListItemsViewerController.java
@@ -0,0 +1,125 @@
+package Controller;
+
+import Model.Game;
+import Model.GameDAO;
+import Model.PurchasableItem;
+import View.GameForm;
+import View.GameInfo;
+import View.ListItemsViewer;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListItemsViewerController implements ChangeListener {
+
+    private static final String PENDING_TAB = "Pending";
+    private static final String PURCHASED_TAB = "Purchased";
+
+    private final ListItemsViewer _viewListItemsViewer;
+    private final GameDAO _gameDAO;
+
+    private DefaultListModel<Game> _defaultListGamesModel;
+
+    public ListItemsViewerController(ListItemsViewer viewListItemsViewer) {
+
+        _viewListItemsViewer = viewListItemsViewer;
+        _gameDAO = new GameDAO();
+
+        _viewListItemsViewer.tabbedPanel.addChangeListener(this);
+        _viewListItemsViewer.addItemB.addActionListener(e -> openGameForm());
+    }
+
+    public void loadTab() {
+
+        if (_viewListItemsViewer.tabbedPanel.getSelectedIndex() != -1) {
+
+            _defaultListGamesModel = new DefaultListModel<>();
+            List<Game> gameList = getGamesList();
+            for (Game game : gameList) {
+
+                _defaultListGamesModel.addElement(game);
+            }
+
+            JList<Game> gameJList = new JList<>(_defaultListGamesModel);
+            gameJList.setCellRenderer(new ItemRenderer());
+            gameJList.addMouseListener(new MouseListener() {
+                public void mouseClicked(MouseEvent e) {
+
+                    GameInfo gi = new GameInfo("GameInfo");
+                    int selectedGameId = gameJList.getSelectedValue().getId();
+
+                    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                    gi.setSize((int) screenSize.getWidth() * 2 / 3, (int) screenSize.getHeight() * 2 / 3);
+
+                    GameInfoController gameInfoController = new GameInfoController(gi, selectedGameId);
+                    gameInfoController.setDefaultListModel(_defaultListGamesModel);
+
+                    gi.setVisible(true);
+                }
+
+                public void mousePressed(MouseEvent e) {
+                }
+
+                public void mouseReleased(MouseEvent e) {
+                }
+
+                public void mouseEntered(MouseEvent e) {
+                }
+
+                public void mouseExited(MouseEvent e) {
+                }
+            });
+
+            gameJList.setBackground(new Color(156, 143, 231));
+            JScrollPane jsp = (JScrollPane) _viewListItemsViewer.tabbedPanel.getSelectedComponent();
+            jsp.setViewportView(gameJList);
+        }
+    }
+
+    private List<Game> getGamesList() {
+
+        List<Game> gameList = new LinkedList<>();
+        if (_viewListItemsViewer.tabbedPanel.getTitleAt(_viewListItemsViewer.tabbedPanel.getSelectedIndex()).equals(PENDING_TAB)) {
+
+            gameList = loadPendingGames();
+        } else if (_viewListItemsViewer.tabbedPanel.getTitleAt(_viewListItemsViewer.tabbedPanel.getSelectedIndex()).equals(PURCHASED_TAB)) {
+
+            gameList = loadPurchasedGames();
+        }
+        return gameList;
+    }
+
+    private List<Game> loadPendingGames() {
+
+        return _gameDAO.getByState(PurchasableItem.ItemState.Pending.toString());
+    }
+
+    private List<Game> loadPurchasedGames() {
+
+        return _gameDAO.getByState(PurchasableItem.ItemState.Purchased.toString());
+    }
+
+    public void stateChanged(ChangeEvent e) {
+
+        loadTab();
+    }
+
+    private void openGameForm() {
+
+        GameForm gf = new GameForm("AddGame");
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        gf.setSize((int) screenSize.getWidth() * 2 / 3, (int) screenSize.getHeight() * 2 / 3);
+
+        GameFormController controller = new GameFormController(gf);
+        controller.setDefaultListModel(_defaultListGamesModel);
+
+        gf.setVisible(true);
+    }
+}
